74ca91a53d58a46fa9a936e3d92dd31a015efe3f
Rahma Sassi <rahmasassi2004@gmail.com>
2025-05-17 18:13:02 +0100

    restructure du projet partie user et partie admin

--- src/Controller/admin/LivreController.php
+++ src/Controller/admin/LivreController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\admin;
 
 use App\Entity\Livre;
 use App\Form\LivreType;
@@ -12,10 +12,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/livre')]
+#[Route('/admin/livre')]
 final class LivreController extends AbstractController
 {
-    #[Route(name: 'app_livre_index', methods: ['GET'])]
+    #[Route(name: 'admin_livre_index', methods: ['GET'])]
     function index(LivreRepository $livreRepository, Request $request, PaginatorInterface $paginator): Response
     {
     $searchTerm = $request->query->get('search');
@@ -28,13 +28,13 @@ final class LivreController extends AbstractController
         12 /* Limite par page */
     );
 
-    return $this->render('livre/index.html.twig', [
+    return $this->render('admin/livre/index.html.twig', [
         'livres' => $livres,
         'searchTerm' => $searchTerm,
     ]);
 }
 
-    #[Route('/new', name: 'app_livre_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'admin_livre_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $livre = new Livre();
@@ -45,24 +45,24 @@ final class LivreController extends AbstractController
             $entityManager->persist($livre);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_livre_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('admin_livre_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('livre/new.html.twig', [
+        return $this->render('admin/livre/new.html.twig', [
             'livre' => $livre,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_livre_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'admin_livre_show', methods: ['GET'])]
     public function show(Livre $livre): Response
     {
-        return $this->render('livre/show.html.twig', [
+        return $this->render('admin/livre/show.html.twig', [
             'livre' => $livre,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_livre_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'admin_livre_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Livre $livre, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(LivreType::class, $livre);
@@ -71,16 +71,16 @@ final class LivreController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_livre_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('admin_livre_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('livre/edit.html.twig', [
+        return $this->render('admin/livre/edit.html.twig', [
             'livre' => $livre,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_livre_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'admin_livre_delete', methods: ['POST'])]
     public function delete(Request $request, Livre $livre, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$livre->getId(), $request->getPayload()->getString('_token'))) {
@@ -88,6 +88,6 @@ final class LivreController extends AbstractController
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_livre_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('admin_livre_index', [], Response::HTTP_SEE_OTHER);
     }
 }
